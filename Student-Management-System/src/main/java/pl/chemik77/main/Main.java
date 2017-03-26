0528ff2e02dad9e0bdd2a489a47848232a2f342b
chemik77 <ania.kawala@gmail.com>
2017-03-26 16:12:41 +0200

    first maven files

---
+++ Student-Management-System/src/main/java/pl/chemik77/main/Main.java
@@ -0,0 +1,20 @@
+package pl.chemik77.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		
+		launch(args);
+		
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
